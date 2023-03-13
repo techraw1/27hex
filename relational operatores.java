4799ed14d4533f4de7291661191eff261cd26db5
Tech Raw <60519938+techraw1@users.noreply.github.com>
2023-03-13 11:53:23 +0100

    Create relational operatores.java
    العمليات العلائقية في الجافا

---
+++ relational operatores.java
@@ -0,0 +1,19 @@
+package techraw;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+// االعمليات العلاقية
+		
+		float a = 10.1f;
+		float b = 0.05f;
+		System.out.println(" a == b: " + (a == b));
+		System.out.println("a != b: " + (a != b));
+		System.out.println("a > b: " + (a > b));
+		System.out.println("a < b: " + (a < b));
+		System.out.println(" a >= b: " + (a >= b));
+		System.out.println("a <= b: " + (a <= b));
+  
+	}
+
+}
