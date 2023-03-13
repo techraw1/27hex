1a398839392b0ae24b48d5e64e3e22b65f71aeac
Tech Raw <60519938+techraw1@users.noreply.github.com>
2023-03-13 11:24:26 +0100

    Create class1calculation.java
    calculation

---
+++ class1calculation.java
@@ -0,0 +1,16 @@
+package techraw;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+  int a = 10;
+  int b = 5;
+  System.out.println("a + b =" + (a + b));
+  System.out.println("a - b =" + (a - b));
+  System.out.println("a × b =" + (a * b));
+  System.out.println("a / b =" + (a / b));
+  System.out.println("a % b =" + (a % b));
+	
+	}
+
+}
