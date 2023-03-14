ad309757a32e3f1c0995fbcf5271a379f065d36d
Tech Raw <60519938+techraw1@users.noreply.github.com>
2023-03-14 19:50:02 +0100

    Create convert.java

---
+++ convert.java
@@ -0,0 +1,30 @@
+package techraw;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+
+		double x,y;
+		byte b;
+		int i;
+		char ch;
+		x = 10.0d;
+		y = 3.0d;
+		i = (int) (x/y);
+		System.out.println(i);
+		i = 100;
+		b = (byte) (i);
+		i = 100;
+		b = (byte) (i);
+		System.out.println(b);
+		i = 399;
+		b = (byte) (i);
+		System.out.println(b);
+		b = 100;
+		ch = (char) (b);
+		System.out.println(ch);
+		
+
+	}
+
+}
